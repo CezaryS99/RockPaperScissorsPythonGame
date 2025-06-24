980a00c6f35faaa2c6e6aa2e813c3e9142ce9ac7
CezaryS99 <c.siedlaczek@gmail.com>
2025-06-24 07:55:05 +0200

    forth  commit

--- app.py
+++ app.py
@@ -28,5 +28,8 @@ def index():
 
     return render_template("index.html", result=result, player=player, computer=computer)
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))  # Render ustawi PORT jako zmienną środowiskową
+    app.run(host="0.0.0.0", port=port)
